8ddce958b41e25da884bdc1ccd94e03500efdb34
Leen Sami Atari <atarileen2003@gmail.com>
2025-03-06 16:45:17 +0300

    changed the bus station name

--- screens/Stations.js
+++ screens/Stations.js
@@ -2,5 +2,5 @@
 export const BUS_STATIONS = [
     { id: 1, name: 'North Amman Bus Station', latitude: 31.994643555305622, longitude: 35.92005357765366 },
     { id: 2, name: 'Ragadan', latitude: 31.962149713630872, longitude: 35.960200566517024 },
-    { id: 3, name: 'South bus station', latitude: 31.918599560221892, longitude:  35.93001946651484 },
+    { id: 3, name: 'South bussss station', latitude: 31.918599560221892, longitude:  35.93001946651484 },
   ];
